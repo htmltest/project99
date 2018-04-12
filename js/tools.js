5c689f65db5ffdaef875937ab80a8f6a03102fc5
htmltest <struev.vs@gmail.com>
2018-04-12 16:48:37 +0300

    update

--- js/tools.js
+++ js/tools.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
 
+    $.validator.addMethod('maskPhone',
+        function(value, element) {
+            if (value == '') {
+                return true;
+            }
+            return /^\+7 \(\d{3}\) \d{3}\-\d{2}\-\d{2}$/.test(value);
+        },
+        'Не соответствует формату'
+    );
+
+    $('form').each(function() {
+        initForm($(this));
+    });
+
     $('.main-events-menu a').click(function(e) {
         var curLi = $(this).parent();
         if (!curLi.hasClass('active')) {
@@ -41,3 +55,89 @@ $(document).ready(function() {
     });
 
 });
+
+(window).on('resize', function() {
+    $('.form-select select').chosen('destroy');
+    $('.form-select select').chosen({disable_search: true, placeholder_text_multiple: ' ', no_results_text: 'Нет результатов'});
+    $('.form-select select').each(function() {
+        var curSelect = $(this);
+        if (curSelect.data('placeholder') != '') {
+            curSelect.parent().find('.chosen-single').prepend('<strong>' + curSelect.data('placeholder') + '</strong>');
+        }
+    });
+});
+
+function initForm(curForm) {
+    curForm.find('input.maskPhone').mask('+7 (999) 999-99-99');
+
+    curForm.find('.form-input input, .form-input textarea').each(function() {
+        if ($(this).val() != '') {
+            $(this).parent().addClass('focus');
+        }
+    });
+
+    curForm.find('.form-input input, .form-input textarea').focus(function() {
+        $(this).parent().addClass('focus');
+    });
+
+    curForm.find('.form-input input, .form-input textarea').blur(function() {
+        if ($(this).val() == '') {
+            $(this).parent().removeClass('focus');
+        }
+    });
+
+    curForm.find('.form-select select').chosen({disable_search: true, no_results_text: 'Нет результатов'});
+    curForm.find('.form-select select').each(function() {
+        var curSelect = $(this);
+        if (curSelect.data('placeholder') != '') {
+            curSelect.parent().find('.chosen-single').prepend('<strong>' + curSelect.data('placeholder') + '</strong>');
+        }
+    });
+
+
+    curForm.find('.form-file input').change(function() {
+        var curInput = $(this);
+        var curField = curInput.parent().parent().parent().parent();
+        curField.find('.form-file-name').html(curInput.val().replace(/.*(\/|\\)/, ''));
+        curField.find('label.error').remove();
+        curField.removeClass('error');
+    });
+
+    curForm.validate({
+        ignore: '',
+        invalidHandler: function(form, validatorcalc) {
+            validatorcalc.showErrors();
+            checkErrors();
+        }
+    });
+}
+
+function checkErrors() {
+    $('.form-checkbox, .form-file, .form-input').each(function() {
+        var curField = $(this);
+        if (curField.find('input.error').length > 0) {
+            curField.addClass('error');
+        } else {
+            curField.removeClass('error');
+        }
+        if (curField.find('input.valid').length > 0) {
+            curField.addClass('valid');
+        } else {
+            curField.removeClass('valid');
+        }
+    });
+
+    $('.form-select').each(function() {
+        var curField = $(this).parent().parent();
+        if (curField.find('select.error').length > 0) {
+            curField.addClass('error');
+        } else {
+            curField.removeClass('error');
+        }
+        if (curField.find('select.valid').length > 0) {
+            curField.addClass('valid');
+        } else {
+            curField.removeClass('valid');
+        }
+    });
+}
